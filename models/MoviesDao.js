afb59279a408c4b0731ab00b368896e3c9bea8c2
nsoarim <99406537+nsoarim@users.noreply.github.com>
2022-04-05 18:29:56 +0900

    Fix:장르와 나라도 가져오도록 쿼리문 수정

--- models/MoviesDao.js
+++ models/MoviesDao.js
@@ -8,14 +8,20 @@ const getPopularMovies = async () => {
   wants.movie_id AS movie_id,
   movies.name as name,
   movies.poster_url as poster_url,
+  movies_genre.genre_name AS genre,
+  movies_country.country_name AS country
   COUNT(movie_id) 
   FROM wants JOIN movies ON wants.movie_id = movies.id
+  JOIN movies_genre ON movies.genre_id = movies_genre.id 
+  JOIN movies_country ON movies.country_id = movies_country.id
   WHERE wants.want=1
   GROUP BY movie_id,want
   order by COUNT(movie_id) desc
   limit 20;
   `;
 };
+
+
 
 const getAvgRating = async (movie_id) => {
   return await prisma.$queryRaw`
