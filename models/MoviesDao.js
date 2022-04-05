5ec70174edfe3316119330a7404fff37b8e4ce29
nsoarim <99406537+nsoarim@users.noreply.github.com>
2022-04-05 18:23:33 +0900

    Fix: 장르와 나라를 가져오도록 쿼리문수정

--- models/MoviesDao.js
+++ models/MoviesDao.js
@@ -6,9 +6,12 @@ const getMoviesByGenre = async genreId => {
     SELECT
     movies.id AS id,
     movies.name AS name,
-    movies.poster_url AS poster_url
+    movies.poster_url AS poster_url,
+    movies_genre.genre_name AS genre,
+    movies_country.country_name AS country
     FROM movies
     JOIN movies_genre ON movies.genre_id = movies_genre.id 
+    JOIN movies_country ON movies.country_id = movies_country.id
     WHERE movies_genre.id = ${genreId}
   `;
 };
