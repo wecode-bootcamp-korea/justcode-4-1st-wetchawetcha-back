c5676391a2f7b12387d8a47061c3bdc280d7e376
wlsun0324 <wlsun0324@gmail.com>
2022-04-06 20:22:24 +0900

    Fix : wnatApi 로그인 여부 기능 합치기

--- routes/WantRoute.js
+++ routes/WantRoute.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const wantController = require("../controllers/WantController");
+const authorMiddleware = require("../controllers/middlewares/Authorization");
 
-router.get("/:movieId", wantController.want);
-router.post("/", wantController.createWant);
-router.patch("/", wantController.updateWant);
+router.get("/:movieId", authorMiddleware.Authorization, wantController.want);
+router.post("/", authorMiddleware.Authorization, wantController.createWant);
+router.patch("/", authorMiddleware.Authorization, wantController.updateWant);
 router.use("/", wantController.error);
 
 module.exports = router;
