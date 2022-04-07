8558b1f1ba895edf68da39fb5e177b56b6f99919
wlsun0324 <wlsun0324@gmail.com>
2022-04-07 14:41:36 +0900

    Add + Del : 초기 test코드 삭제 및 error 기능 추가

---
+++ controllers/ErrorController.js
@@ -0,0 +1,12 @@
+
+const error = (req, res, next) => {
+    console.error("*****************************************************************************************************************");
+    console.error("Request-METHOD : " + req.method);
+    console.error("Request-RUI : " + req.url);
+    console.error("User-Agent : " + req.header('User-Agent'));
+    console.error("*****************************************************************************************************************");
+    
+    res.status(404).json({message : "Bad-Request-URL"});
+}
+
+module.exports = { error }
